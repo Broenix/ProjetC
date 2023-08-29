55eb403a53707abb9d089bed7e0386cc1ea1c0cb
Theo Chevalier <chevalie.theo@gmail.com>
2023-08-29 10:04:25 +0200

    ca fonctionne mais c'est pas vraiment ce qu'on nous demande

--- main.c
+++ main.c
@@ -1,8 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
+#define MAX_LEN 100
+
+void readPerroq(char *perroquet) {
+    FILE *file = fopen("peroq.def", "r");
+    if (file == NULL) {
+        printf("Erreur au niveau de la lecture du perroq");
+        exit(1);
+    }
+    fscanf(file, "%s", perroquet);
+    fclose(file);
+}
+
+void savePerroq(const char *perroquet) {
+    FILE *file = fopen("peroq.def", "w");
+    if (file == NULL) {
+        printf("Erreur au niveau de la sauvegarde du perroq");
+        exit(1);
+    }
+    fprintf(file, "%s", perroquet);
+    fclose(file);
+}
+
+void chiffrePerroq(const char *source, const char *perroquet, char *chiffree) {
+    int sourceLen = strlen(source);
+    int perroquetLen = strlen(perroquet);
+    int i = 0; //c'est moche mais ça fonctionne
+
+    for (i = 0; i < sourceLen; i++) {
+        chiffree[i] = source[i] + perroquet[i % perroquetLen];
+    }
+    chiffree[sourceLen] = '\0';
+}
+
+void dechiffrePerroq(const char *chiffree, const char *perroquet, char *dechiffree) {
+    int chiffreeLen = strlen(chiffree);
+    int perroquetLen = strlen(perroquet);
+    int i = 0;
+
+    for (i = 0; i < chiffreeLen; i++) {
+        dechiffree[i] = chiffree[i] - perroquet[i % perroquetLen];
+    }
+    dechiffree[chiffreeLen] = '\0';
+}
+
+int main() {
+    char perroquet[MAX_LEN];
+    char source[MAX_LEN];
+    char chiffree[MAX_LEN];
+    char dechiffree[MAX_LEN];
+
+    printf("Programme de chiffrement 'Perroquet'!\n");
+
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Choisir le perroquet\n");
+        printf("2. Chiffrer un texte\n");
+        printf("3. Dechiffrer un texte\n");
+        printf("4. Quitter\n");
+
+        int choix;
+        printf("Entrez votre choix: ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                printf("Entrez le perroquet : ");
+                scanf("%s", perroquet);
+                savePerroq(perroquet);
+                printf("Perroquet enregistre.\n");
+                break;
+            case 2:
+                printf("Entrez le texte à chiffrer : ");
+                scanf(" %[^\n]s", source);
+
+                readPerroq(perroquet);
+                chiffrePerroq(source, perroquet, chiffree);
+
+                printf("Texte chiffre : %s\n", chiffree);
+                break;
+            case 3:
+                printf("Entrez le texte chiffre : ");
+                scanf(" %[^\n]s", dechiffree);
+
+                readPerroq(perroquet);
+                dechiffrePerroq(chiffree, perroquet, dechiffree);
+
+                printf("Texte déchiffre : %s\n", dechiffree);
+                break;
+            case 4:
+                printf("Au revoir !\n");
+                return 0;
+            default:
+                printf("Choix invalide. Veuillez entrer un choix valide.\n");
+        }
+    }
+
     return 0;
 }
